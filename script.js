968ee87e6a981c2e21be5b939dc07442a14add6a
babi bhaumik <babibhaumik2006@gmail.com>
2026-07-19 10:29:55 +0530

    ni

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-array=["apple","man","Women","ROBOT"];
-array.forEach(cap)
-array.forEach(display)
-
-function display(element){
-console.log(element);
-}
-function cap(element,index,array){
-array[index]=element.charAt(0).toUpperCase()+element.slice(1).toLowerCase();
-}
+const button=document.getElementById('btn');
+button.addEventListener('click', function() {
+    alert('Submit button needs backend to work, you may help me with that and you may also buy subdomain at 10 rs per month');
+});
