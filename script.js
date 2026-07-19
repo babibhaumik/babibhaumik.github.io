51847c2dc424d591a623ba10e8bd953d0a66ddce
babi bhaumik <babibhaumik2006@gmail.com>
2026-07-19 19:32:03 +0530

    making it kokborok translator

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
-const button=document.getElementById('btn');
-button.addEventListener('click', function() {
-    alert('Submit button needs backend to work, you may help me with that and you may also buy subdomain at 10 rs per month');
+let dict = {
+    "hi": "Khulumlkha",
+    "i": "ang",
+    "you": "nwng",
+    "how": "bahai",
+    "what": "tamo",
+    "he": "bo",
+    "do": "khai",
+    "eat": "cha",
+    "we":"chwng",
+    "me":"ano",
+    "they":"borok",
+    "y'all":"norok",
+    "go":"thang",
+    "come":"phai",
+    "goood":"kaham",
+    "rice":"mai",
+    "school":"rwngnok",
+    "home":"nok"
+};
+
+const skipWords = ["am", "is", "are"];
+
+const button = document.getElementById("btn");
+
+button.addEventListener("click", function () {
+
+    const input = document.getElementById("userInput").value;
+
+    const words = input.toLowerCase().split(/\s+/);
+
+    let translated = [];
+
+    for (let word of words) {
+
+        if (skipWords.includes(word)) {
+            continue;       // Skip am, is, are
+        }
+
+        translated.push(dict[word] || word);
+    }
+
+    document.getElementById("result").innerText = translated.join(" ");
 });
+
